553a0a249804ee36ac66f65a73a1f13315b9a7bc
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2019-07-10 16:53:15 +0800

    remove unused code

--- src/pages/Reception/Appointment/Calendar/Appointments/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppointmentContainer } from './Container'
-export { default as AppointmentContent } from './Content'
-export { default as AppointmentComponent } from './Component'
-export { default as AptFormPopUp } from './AppointmentForm'
