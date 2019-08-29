8ae285ffc2223404e5c0e90c3b2ee8e0512dd2e0
Kai Sian <wong_kai_sian@medinno.onmicrosoft.com>
2019-08-29 14:40:17 +0800

    Fix Bug 9799

---
+++ src/pages/PatientDatabase/models/streetAddress.js
@@ -0,0 +1,51 @@
+import { createFormViewModel } from 'medisys-model'
+import * as service from '../Detail/services/streetAddress'
+import { notification } from '@/components'
+
+export default createFormViewModel({
+  namespace: 'streetAddress',
+  config: {
+    // queryOnLoad: false,
+  },
+  param: {
+    service,
+    state: {
+      default: {
+        postalCode: '',
+        blkHseNo: '',
+        street: '',
+        building: '',
+        houseType: '',
+      },
+    },
+
+    subscriptions: ({ dispatch, history }) => {
+      history.listen(async (loct, method) => {
+        const { pathname, search, query = {} } = loct
+      })
+    },
+    effects: {
+      *fetchAddress ({ payload }, { call, put }) {
+        const response = yield call(service.queryList, payload)
+        if (response.data.data.length === 0)
+          return (
+            notification.warn({ message: 'Address Not Found' }),
+            {
+              data: [
+                {
+                  postalCode: '',
+                  blkHseNo: '',
+                  street: '',
+                  building: '',
+                  houseType: '',
+                },
+              ],
+            }
+          )
+
+        return response.data
+      },
+    },
+    reducers: {},
+  },
+})
