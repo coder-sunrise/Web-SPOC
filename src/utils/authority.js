0f26c0b57c5a704ffd588e4a1e49ff66d8bb50a5
Kai Sian <wong_kai_sian@medinno.onmicrosoft.com>
2019-10-07 19:11:08 +0800

    Fix bug 10817, Change disabled with AuthorizedContext

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -13,6 +13,8 @@ const roleAccessRightsMapping = {
     { name: 'dispense.edit', rights: 'enable' },
     { name: 'schemeDetail.view', rights: 'enable' },
     { name: 'schemeDetail.edit', rights: 'enable' },
+    { name: 'settingCompany.view', rights: 'enable' },
+    { name: 'settingCompany.edit', rights: 'enable' },
   ],
   editor: [
     { name: 'editor', rights: 'enable' },
