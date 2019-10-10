d83edb937ec22cb63ce6e7e902f0c97d5d68a6ad
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2019-10-10 16:06:02 +0800

    fix queue listing bug

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
+const initialState = {
+  user: {},
+  login: {},
+}
+
 export const dva = {
   config: {
     onError (e) {
       e.preventDefault()
       console.log(e.message)
     },
-    // onReducer: (reducer) => {
-    //   console.log({ reducer })
-    //   // return {...state}
+    // onReducer (reducer) {
+    //   return (state, action) => {
+    //     // const newState =
+    //     //   action.type === 'RESET' ? initialState : reducer(state, action)
+    //     const newState =
+    //       action.type === 'RESET' ? initialState : reducer(state, action)
+    //     return { ...newState, routing: newState.routing }
+    //   }
     // },
   },
   // plugins: [
