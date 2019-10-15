aa51685d0a63d798fcc5dbb3d8a9825b0c69fb33
Kai Sian <wong_kai_sian@medinno.onmicrosoft.com>
2019-10-15 16:30:45 +0800

    FIx bug 10909 & 10907

--- src/pages/Setting/ServiceCategory/Grid.js
+++ src/pages/Setting/ServiceCategory/Grid.js
@@ -1,10 +1,7 @@
 import React, { PureComponent } from 'react'
-
-import { CommonTableGrid, Button } from '@/components'
-import { Table } from '@devexpress/dx-react-grid-material-ui'
-import Delete from '@material-ui/icons/Delete'
 import Edit from '@material-ui/icons/Edit'
-import * as service from './services'
+import { CommonTableGrid, Button, Tooltip } from '@/components'
+import { status } from '@/utils/codes'
 
 class Grid extends PureComponent {
   editRow = (row, e) => {
@@ -40,6 +37,7 @@ class Grid extends PureComponent {
           { name: 'code', title: 'Code' },
           { name: 'displayValue', title: 'Display Value' },
           { name: 'description', title: 'Description' },
+          { name: 'isActive', title: 'Status' },
           {
             name: 'action',
             title: 'Action',
@@ -48,22 +46,32 @@ class Grid extends PureComponent {
         // FuncProps={{ pager: false }}
         columnExtensions={[
           {
+            columnName: 'isActive',
+            sortingEnabled: false,
+            type: 'select',
+            options: status,
+            align: 'center',
+            width: 100,
+          },
+          {
             columnName: 'action',
             sortingEnabled: false,
             align: 'center',
             render: (row) => {
               return (
-                <Button
-                  size='sm'
-                  onClick={() => {
-                    this.editRow(row)
-                  }}
-                  justIcon
-                  color='primary'
-                  disabled={!row.isUserMaintainable}
-                >
-                  <Edit />
-                </Button>
+                <Tooltip title='Edit Service Category'>
+                  <Button
+                    size='sm'
+                    onClick={() => {
+                      this.editRow(row)
+                    }}
+                    justIcon
+                    color='primary'
+                    disabled={!row.isUserMaintainable}
+                  >
+                    <Edit />
+                  </Button>
+                </Tooltip>
               )
             },
           },
