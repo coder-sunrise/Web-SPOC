a611dd7e9553c4c32090b7be51d129f57f8b97f4
Kai Sian <wong_kai_sian@medinno.onmicrosoft.com>
2019-10-18 14:10:02 +0800

    Fix bug 11174, Not do unsave change check

--- src/pages/Finance/Statement/NewStatement/AddNewStatement.js
+++ src/pages/Finance/Statement/NewStatement/AddNewStatement.js
@@ -58,8 +58,7 @@ const styles = () => ({
     paymentTerm: Yup.number().required(),
   }),
 
-  notDirtyDuration: 3,
-  handleSubmit: (values, { props }) => {
+  handleSubmit: (values, { props, resetForm }) => {
     const { effectiveDates, ...restValues } = values
     const { dispatch, history } = props
     console.log('submit', values)
@@ -71,6 +70,7 @@ const styles = () => ({
       },
     }).then((r) => {
       if (r) {
+        resetForm()
         history.push('/finance/statement')
       }
     })
@@ -223,7 +223,9 @@ class AddNewStatement extends PureComponent {
   }
 
   goBackToPreviousPage = () => {
-    this.props.history.goBack()
+    const { history, resetForm } = this.props
+    resetForm()
+    history.goBack()
   }
 
   render () {
