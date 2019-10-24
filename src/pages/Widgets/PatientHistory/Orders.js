a5958c6493ee36bb99cad939ac56e965d1a465bd
Chin Wai Keat <chin_wai_keat@medinno.onmicrosoft.com>
2019-10-24 13:59:55 +0800

    clear unsed code

--- src/pages/Widgets/PatientHistory/Orders.js
+++ src/pages/Widgets/PatientHistory/Orders.js
@@ -12,19 +12,6 @@ export default ({ current }) => (
     ]}
     FuncProps={{ pager: false }}
     columnExtensions={[
-      {
-        columnName: 'type',
-        // type: 'select',
-        // options: orderTypes,
-        render: (r) => {
-          return (
-            <div>
-              {r.type} 
-              {r.isExternalPrescription === true ? <span> (Ext.) </span> : ''}
-            </div>
-          )
-        },
-      },
       { columnName: 'totalAmount', type: 'number', currency: true },
     ]}
   />
