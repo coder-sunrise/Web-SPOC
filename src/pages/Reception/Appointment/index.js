c6546d431e37198e6cd9c53ff515dc36b0bd2568
iaiy <4242490@qq.com>
2019-10-26 11:45:37 +0800

    11315 Appointment - Show each doctor name in the calendar when All doctor is selected.

--- src/pages/Reception/Appointment/index.js
+++ src/pages/Reception/Appointment/index.js
@@ -286,24 +286,23 @@ class Appointment extends React.PureComponent {
   onFilterUpdate = (filter) => {
     const { filterByDoctor = [] } = filter
     const { clinicianProfiles } = this.props
-    console.log(clinicianProfiles)
-    const newResources = filterByDoctor.includes(-99)
-      ? []
-      : clinicianProfiles.reduce(
-          (resources, doctor) =>
-            filterByDoctor.includes(doctor.id)
-              ? [
-                  ...resources,
-                  {
-                    clinicianFK: doctor.id,
-                    doctorName: doctor.name,
-                  },
-                ]
-              : [
-                  ...resources,
-                ],
-          [],
-        )
+
+    const newResources = clinicianProfiles.reduce(
+      (resources, doctor) =>
+        filterByDoctor.includes(doctor.id)
+          ? [
+              ...resources,
+              {
+                clinicianFK: doctor.id,
+                doctorName: doctor.name,
+              },
+            ]
+          : [
+              ...resources,
+            ],
+      [],
+    )
+
     this.setState((preState) => ({
       filter: { ...preState.filter, ...filter },
       resources: newResources.length > 0 ? newResources : null,
