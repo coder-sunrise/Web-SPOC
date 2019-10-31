6ab43d42379bef0d1f5c0eb5a5f9f8122bffb85c
Kai Sian <wong_kai_sian@medinno.onmicrosoft.com>
2019-10-31 17:31:17 +0800

    Initial integration on SMS module

---
+++ src/pages/SMS/services/index.js
@@ -0,0 +1,34 @@
+import * as service from '@/services/common'
+import { convertToQuery } from '@/utils/utils'
+import request from '@/utils/request'
+
+const url = '/api/SMSReminder'
+
+module.exports = {
+  querySMSData: async (params, smsType) => {
+    const parsedParams = convertToQuery({
+      pagesize: 10,
+      current: 1,
+      ...params,
+    })
+    const data = await request(`${url}/${smsType}`, {
+      method: 'GET',
+      data: parsedParams,
+    })
+
+    return data
+  },
+  query: (params) => {
+    return service.query(url, params)
+  },
+
+  upsert: async (params) => {
+    const r = await request(url, {
+      method: 'PUT',
+      body: [
+        ...params,
+      ],
+    })
+    return r
+  },
+}
---
+++ src/pages/SMS/variables.js
@@ -0,0 +1,134 @@
+import moment from 'moment'
+import Grid from './Grid'
+import {
+  DateFormatter,
+  dateFormatLong,
+  timeFormat24HourWithSecond,
+  timeFormatSmallCase,
+} from '@/components'
+
+const appointmentColumns = [
+  { name: 'patientName', title: 'Patient Name' },
+  { name: 'patientContactNo', title: 'Contact No' },
+  { name: 'upcomingAppointmentDate', title: 'Upcoming Appt.' },
+  { name: 'appointmentRemarks', title: 'Appt. Remarks' },
+  { name: 'doctor', title: 'Doctor' },
+  { name: 'appointmentStatus', title: 'Appt. Status' },
+  { name: 'appointmentType', title: 'Appt. Type' },
+  { name: 'lastVisitDate', title: 'Last Visit Date' },
+  { name: 'lastSMSSendStatus', title: 'Last SMS Status' },
+  { name: 'lastSMSSendDate', title: 'Last SMS Sent' },
+  { name: 'isReminderSend', title: 'Reminder Sent' },
+  { name: 'Action', title: 'Action' },
+]
+
+const patientColumns = [
+  { name: 'patientName', title: 'Patient Name' },
+  { name: 'patientContactNo', title: 'Contact No' },
+  { name: 'doctor', title: 'Doctor' },
+  { name: 'lastVisitDate', title: 'Last Visit Date' },
+  { name: 'lastSMSSendStatus', title: 'Last SMS Status' },
+  { name: 'lastSMSSentDate', title: 'Last SMS Sent' },
+  { name: 'Action', title: 'Action' },
+]
+
+const appointmentColumnsExtensions = [
+  {
+    columnName: 'upcomingAppointmentDate',
+    width: 180,
+    render: (row) => {
+      const { upcomingAppointmentDate, upcomingAppointmentStartTime } = row
+      return `${moment(upcomingAppointmentDate).format(
+        dateFormatLong,
+      )} ${moment(
+        upcomingAppointmentStartTime,
+        timeFormat24HourWithSecond,
+      ).format(timeFormatSmallCase)} `
+    },
+  },
+  {
+    columnName: 'lastVisitDate',
+    width: 180,
+    render: (row) =>
+      DateFormatter({
+        value: row.lastVisitDate,
+        full: true,
+      }),
+  },
+  {
+    columnName: 'lastSMSSendDate',
+    width: 180,
+    render: (row) =>
+      DateFormatter({
+        value: row.lastSMSSendDate,
+        full: true,
+      }),
+  },
+  {
+    columnName: 'Action',
+    width: 100,
+    align: 'center',
+  },
+]
+
+const patientColumnsExtensions = [
+  {
+    columnName: 'lastVisitDate',
+    width: 180,
+    render: (row) =>
+      DateFormatter({
+        value: row.lastVisitDate,
+        full: true,
+      }),
+  },
+  {
+    columnName: 'lastSMSSendDate',
+    width: 180,
+    render: (row) =>
+      DateFormatter({
+        value: row.lastSMSSendDate,
+        full: true,
+      }),
+  },
+  {
+    columnName: 'Action',
+    width: 100,
+    align: 'center',
+  },
+]
+
+const appointmentProps = {
+  type: 'Appointment',
+  columns: appointmentColumns,
+  columnsExtensions: appointmentColumnsExtensions,
+}
+
+const patientProps = {
+  type: 'Patient',
+  columns: patientColumns,
+  columnsExtensions: patientColumnsExtensions,
+}
+
+const addContent = (type, props) => {
+  switch (type) {
+    case 1:
+      return <Grid {...props} {...appointmentProps} />
+    case 2:
+      return <Grid {...props} {...patientProps} />
+    default:
+      return <Grid {...props} />
+  }
+}
+
+export const SmsOption = (detailsProps) => [
+  {
+    id: 0,
+    name: 'Appointment',
+    content: addContent(1, detailsProps),
+  },
+  {
+    id: 1,
+    name: 'Patient',
+    content: addContent(2, detailsProps),
+  },
+]
