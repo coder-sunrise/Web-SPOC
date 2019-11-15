97dcf3ca810104bd80c31e95a185dcd037cc6142
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2019-11-15 15:17:13 +0800

    add item adjustment column to dispense details tables

--- src/pages/Dispense/Main.js
+++ src/pages/Dispense/Main.js
@@ -199,6 +199,7 @@ class Main extends Component {
 
   render () {
     const { classes, handleSubmit } = this.props
+    console.log({ values: this.props.values })
     return (
       <div className={classes.root}>
         <GridContainer direction='column' className={classes.content}>
