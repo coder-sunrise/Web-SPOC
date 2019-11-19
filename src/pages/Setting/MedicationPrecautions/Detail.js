1616efb3c83b13c237019f12b441f6a469458ca2
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2019-11-19 15:28:14 +0800

    fix 11933: must select languageFK after set translationdisplayvalue and disabel TranslatedDisplayValue sorting

--- src/pages/Setting/MedicationPrecautions/Detail.js
+++ src/pages/Setting/MedicationPrecautions/Detail.js
@@ -33,9 +33,13 @@ const styles = (theme) => ({})
     translationLink: Yup.object().shape({
       translationMasters: Yup.array().of(
         Yup.object().shape({
-          displayValue: Yup.string().when('languageFK', {
+          displayValue: Yup.string().when('tempLanguageFK', {
             is: (v) => v !== undefined,
             then: Yup.string().required(),
+          }),
+          languageFK: Yup.number().when('displayValue', {
+            is: (v) => v !== undefined && v !== null && v.trim() !== '',
+            then: Yup.number().required(),
           }),
         }),
       ),
@@ -68,7 +72,7 @@ class Detail extends PureComponent {
 
   render () {
     const { props } = this
-    const { theme, footer, settingMedicationPrecautions } = props
+    const { theme, footer, settingMedicationPrecautions, setFieldValue } = props
     // console.log('detail', props)
     return (
       <React.Fragment>
@@ -138,6 +142,12 @@ class Detail extends PureComponent {
                     <CodeSelect
                       label='Translation Language'
                       code='ctLanguage'
+                      onChange={(value) => {
+                        setFieldValue(
+                          `translationLink.translationMasters[0].tempLanguageFK`,
+                          value,
+                        )
+                      }}
                       {...args}
                     />
                   )
