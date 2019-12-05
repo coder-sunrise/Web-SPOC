43d1e855243259448b6791c03c708e4b8b085f78
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2019-12-05 15:53:17 +0800

    change all text field contact number to use MobileNumberInput component

--- src/components/_medisys/MobileNumberInput/index.js
+++ src/components/_medisys/MobileNumberInput/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 // common components
 import { NumberInput } from '@/components'
 
-const MobileNumberInput = ({ ...props }) => (
+const MobileNumberInput = ({ label = 'Contact No.', ...props }) => (
   <NumberInput
     {...props}
-    label='Contact No.'
+    label={label}
     max={999999999999999}
+    min={0}
     maxLength={15}
     precision={0}
   />
