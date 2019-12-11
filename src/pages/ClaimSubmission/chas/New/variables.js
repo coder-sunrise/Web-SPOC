6acafd4e65d2d84128b4f717986da36eae649aa2
Lye Yong Sheng <LyeYongSeng@medinno.onmicrosoft.com>
2019-12-11 18:47:03 +0800

    1) Add CHas Claim Status Filter to Approved Tab 2) Change to Invoice Date Range filter 3) Update sorting value for Invoice Claim

--- src/pages/ClaimSubmission/chas/New/variables.js
+++ src/pages/ClaimSubmission/chas/New/variables.js
@@ -56,8 +56,8 @@ export const NewCHASColumns = [
 export const NewCHASColumnExtensions = [
   { columnName: 'visitDate', type: 'date' },
   { columnName: 'invoiceDate', type: 'date' },
-  { columnName: 'rejectionReason', width: 400 },
+  { columnName: 'rejectionReason', width: 400, sortBy: 'ResponseStatusDescription' },
   { columnName: 'schemeCategoryDisplayValue', width: 145 , sortBy:'schemeCategory'},
-  { columnName: 'invoiceAmount', type: 'currency', currency: true },
-  { columnName: 'claimAmount', type: 'currency', currency: true },
+  { columnName: 'invoiceAmount', type: 'currency', currency: true, sortBy:'invoiceAmt' },
+  { columnName: 'claimAmount', type: 'currency', currency: true , sortBy:'claimAmt'},
 ]
