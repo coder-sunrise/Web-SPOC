62a9fc0d7805126ba5e70e11f6adbadf5759da61
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2019-12-24 11:05:13 +0800

    add notification timestamp

--- src/utils/realtime.js
+++ src/utils/realtime.js
@@ -112,6 +112,7 @@ const sendNotification = (type, data) => {
     },
   } = getState()
   data.sender = user.data.clinicianProfile.name
+  data.timestamp = Date.now()
   console.log('sendNotification', type, data)
   connection.invoke('SendNotification', type, data).catch((err) => {
     return console.error(err)
