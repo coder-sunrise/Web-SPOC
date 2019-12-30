1efd02a54aef54c8713874f6d3efc23f0e15b5b3
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2019-12-30 15:40:55 +0800

    12872 Queue - Bill-First visit should allow doctor to edit consultation when the first COR version is saved in DB.

--- src/utils/realtime.js
+++ src/utils/realtime.js
@@ -116,7 +116,7 @@ const sendNotification = (type, data) => {
   data.sender = user.data.clinicianProfile.name
   data.senderId = user.data.id
   data.timestamp = Date.now()
-  console.log('sendNotification', type, data)
+
   connection.invoke('SendNotification', type, data).catch((err) => {
     return console.error(err)
   })
