3ae6f6b3fb8b335211ea628883a235ffaf5af6ab
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2020-01-11 12:28:37 +0800

    add system user authority check in setting page

--- src/pages/Setting/index.js
+++ src/pages/Setting/index.js
@@ -223,6 +223,7 @@ const Authority = {
   'Master Setting': 'settings.mastersetting',
   'Clinic Setting': 'settings.clinicsetting',
   'Print Setup': 'settings.printsetup',
+  'System User': 'settings.systemuser',
   'User Preference': 'settings.userpreference',
   Templates: 'settings.template',
   Contact: 'settings.contact',
@@ -237,7 +238,7 @@ class SystemSetting extends PureComponent {
   constructor (props) {
     super(props)
     this.group = _.groupBy(menuData, 'title')
-    console.log({ group: this.group })
+
     // console.log(menuData, group, Object.keys(group))
     const { classes, theme } = props
   }
