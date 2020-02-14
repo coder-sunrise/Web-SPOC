9154b631ce0b87c3e8f82910995466a2eb5c6752
Zhenyuan <luzhenyuan@medinno.onmicrosoft.com>
2020-02-14 12:08:20 +0800

    changes to the dev notifications. Some qid unable to get.

--- src/components/mui-pro/Header/HeaderLinks.js
+++ src/components/mui-pro/Header/HeaderLinks.js
@@ -187,8 +187,8 @@ class HeaderLinks extends React.Component {
               <div
                 style={{
                   width: '100%',
-                  minWidth: 300,
-                  maxWidth: 400,
+                  minWidth: 400,
+                  maxWidth: 500,
                   maxHeight: 290,
                   overflowY: 'auto',
                 }}
@@ -255,8 +255,8 @@ class HeaderLinks extends React.Component {
           <div
             style={{
               width: '100%',
-              minWidth: 300,
-              maxWidth: 400,
+              minWidth: 400,
+              maxWidth: 500,
               minHeight: 290,
               maxHeight: 290,
               display: 'flex',
@@ -279,6 +279,14 @@ class HeaderLinks extends React.Component {
             <p style={{ margin: 'auto auto auto 15px', fontSize: 16 }}>
               Notifications
             </p>
+            <a
+              style={{ margin: 'auto 15px auto auto', fontSize: 16 }}
+              onClick={() => {
+                this.forceUpdate()
+              }}
+            >
+              Refresh Q
+            </a>
           </div>
           {NotificationContent()}
         </div>
