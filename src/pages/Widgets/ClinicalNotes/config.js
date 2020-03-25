7a3685e5c1fdee412e5389b1b11e811cfd808479
Lim Zhe Bin <lim_zhe_bin@medinno.com>
2020-03-25 14:57:22 +0800

    change clinical notes widget field entry sequence

--- src/pages/Widgets/ClinicalNotes/config.js
+++ src/pages/Widgets/ClinicalNotes/config.js
@@ -3,6 +3,16 @@ import { SCRIBBLE_NOTE_TYPE, CANNED_TEXT_TYPE } from '@/utils/constants'
 export const defaultConfigs = {
   fields: [
     {
+      authority: 'queue.consultation.clinicalnotes.associatedhistory',
+      category: 'History',
+      fieldName: 'history',
+      fieldTitle: 'History',
+      scribbleField: 'historyScribbleArray',
+      scribbleNoteTypeFK: SCRIBBLE_NOTE_TYPE.HISTORY,
+      cannedTextTypeFK: CANNED_TEXT_TYPE.HISTORY,
+      index: 0,
+    },
+    {
       authority: 'queue.consultation.clinicalnotes.chiefcomplaints',
       category: 'ChiefComplaints',
       fieldName: 'chiefComplaints',
@@ -11,7 +21,7 @@ export const defaultConfigs = {
       scribbleNoteTypeFK: SCRIBBLE_NOTE_TYPE.CHIEFCOMPLAINTS,
       cannedTextTypeFK: CANNED_TEXT_TYPE.CHIEFCOMPLAINTS,
       height: 390,
-      index: 0,
+      index: 1,
     },
     {
       authority: 'queue.consultation.clinicalnotes.clinicalnotes',
@@ -21,7 +31,7 @@ export const defaultConfigs = {
       scribbleField: 'notesScribbleArray',
       scribbleNoteTypeFK: SCRIBBLE_NOTE_TYPE.CLINICALNOTES,
       cannedTextTypeFK: CANNED_TEXT_TYPE.NOTE,
-      index: 1,
+      index: 2,
     },
     {
       authority: 'queue.consultation.clinicalnotes.plan',
@@ -31,16 +41,6 @@ export const defaultConfigs = {
       scribbleField: 'planScribbleArray',
       scribbleNoteTypeFK: SCRIBBLE_NOTE_TYPE.PLAN,
       cannedTextTypeFK: CANNED_TEXT_TYPE.PLAN,
-      index: 2,
-    },
-    {
-      authority: 'queue.consultation.clinicalnotes.associatedhistory',
-      category: 'History',
-      fieldName: 'history',
-      fieldTitle: 'History',
-      scribbleField: 'historyScribbleArray',
-      scribbleNoteTypeFK: SCRIBBLE_NOTE_TYPE.HISTORY,
-      cannedTextTypeFK: CANNED_TEXT_TYPE.HISTORY,
       index: 3,
     },
   ],
