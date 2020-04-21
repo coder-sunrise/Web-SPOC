49bc797be315375e2d5332ec0863bfac8522fba0
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2020-04-21 11:34:48 +0800

    14626-Patient History - Vaccination UOM column is missing under dispense tab.

--- src/pages/Widgets/DispenseHistory/Vaccination.js
+++ src/pages/Widgets/DispenseHistory/Vaccination.js
@@ -2,36 +2,31 @@ import React from 'react'
 import { CardContainer, CommonTableGrid } from '@/components'
 
 export const tableColumns = [
-  {name:'visitDate', title:'Date'},
-  {name:'code', title:'Code'},
-  {name:'name', title:'Name'},
-  {name:'remarks', title:'Remarks'},
-  {name:'dispensedQuanity', title:'Qty'},
-  {name:'totalPrice', title:'Subtotal'},
-  {name:'adjAmt', title:'Adjustment'},
-  {name:'totalAfterItemAdjustment', title:'Total'},
+  { name: 'visitDate', title: 'Date' },
+  { name: 'code', title: 'Code' },
+  { name: 'name', title: 'Name' },
+  { name: 'remarks', title: 'Remarks' },
+  { name: 'dispensedQuanity', title: 'Qty' },
+  { name: 'dispenseUOM', title: 'UOM' },
+  { name: 'totalPrice', title: 'Subtotal' },
+  { name: 'adjAmt', title: 'Adjustment' },
+  { name: 'totalAfterItemAdjustment', title: 'Total' },
 ]
 
-export const TableColumnExtensions =
-  [
-    { columnName: 'visitDate', type: 'date' },
-    { columnName: 'totalPrice', type: 'currency'},
-    { columnName: 'adjAmt', type: 'currency'},
-    { columnName: 'totalAfterItemAdjustment', type: 'currency'},
-    { columnName: 'dispensedQuanity', type: 'number'},
-  ]
+export const TableColumnExtensions = [
+  { columnName: 'visitDate', type: 'date' },
+  { columnName: 'totalPrice', type: 'currency' },
+  { columnName: 'adjAmt', type: 'currency' },
+  { columnName: 'totalAfterItemAdjustment', type: 'currency' },
+  { columnName: 'dispensedQuanity', type: 'number' },
+]
 
 export default ({ classes, current, fieldName = '' }) => {
-
   return (
-    <CardContainer
-      hideHeader
-      size='sm'
-      style={{margin:0}}
-    >
+    <CardContainer hideHeader size='sm' style={{ margin: 0 }}>
       <CommonTableGrid
         size='sm'
-        rows={current.vaccination||[]}
+        rows={current.vaccination || []}
         columns={tableColumns}
         FuncProps={{ pager: false }}
         columnExtensions={TableColumnExtensions}
