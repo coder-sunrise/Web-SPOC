19717abf0214ea202bdb695fb7aa2b9db3185d1c
Yang Feixiang <YangFeiXiang@medinno.onmicrosoft.com>
2020-07-31 10:35:09 +0800

    deposit access right control

--- src/pages/PatientDatabase/Detail/PatientDeposit/FilterBar.js
+++ src/pages/PatientDatabase/Detail/PatientDeposit/FilterBar.js
@@ -86,7 +86,9 @@ class FilterBar extends PureComponent {
 
   render () {
     const { isDeposit, showDepositRefundModal } = this.state
+    const { disabled } = this.props
     const patientId = Number(findGetParameter('pid'))
+
     return (
       <React.Fragment>
         <GridItem md={3}>
@@ -114,17 +116,20 @@ class FilterBar extends PureComponent {
               onClick={() => {
                 this.addDepositRefund(true)
               }}
+              disabled={disabled}
               color='primary'
             >
               Deposit
             </Button>
           </Authorized>
+
           <Authorized authority='deposit.refund'>
             <Button
               size='lg'
               onClick={() => {
                 this.addDepositRefund(false)
               }}
+              disabled={disabled}
               color='primary'
             >
               Refund
