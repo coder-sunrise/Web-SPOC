f255937ce6259e9631076c964296318d1fccbaa6
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2020-08-06 17:57:36 +0800

    remove debug code

--- src/pages/PatientDatabase/Detail/EmergencyContact/index.js
+++ src/pages/PatientDatabase/Detail/EmergencyContact/index.js
@@ -20,7 +20,6 @@ class EmergencyContact extends PureComponent {
   }
 
   tableParas = {
-    extraColumn: [],
     columns: [
       { name: 'accountNoTypeFK', title: 'Account Type' },
       { name: 'accountNo', title: 'Account No' },
