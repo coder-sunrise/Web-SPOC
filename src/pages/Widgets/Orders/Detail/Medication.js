d814c82c27962f2f4a91c3c71f7aea0f9504cc33
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2020-11-18 10:11:05 +0800

    17269: total is empty when change duration in order cart

--- src/pages/Widgets/Orders/Detail/Medication.js
+++ src/pages/Widgets/Orders/Detail/Medication.js
@@ -619,10 +619,48 @@ class Medication extends PureComponent {
       corPrescriptionItemInstruction = [],
       corPrescriptionItemPrecaution = [],
     } = values
-    const defaultInstruction = {
+    let defaultInstruction = {
       sequence: 0,
       stepdose: 'AND',
     }
+
+    if (op.id) {
+      defaultInstruction = {
+        ...defaultInstruction,
+        usageMethodFK: op.medicationUsage ? op.medicationUsage.id : undefined,
+        usageMethodCode: op.medicationUsage
+          ? op.medicationUsage.code
+          : undefined,
+        usageMethodDisplayValue: op.medicationUsage
+          ? op.medicationUsage.name
+          : undefined,
+        dosageFK: op.prescribingDosage ? op.prescribingDosage.id : undefined,
+        dosageCode: op.prescribingDosage
+          ? op.prescribingDosage.code
+          : undefined,
+        dosageDisplayValue: op.prescribingDosage
+          ? op.prescribingDosage.name
+          : undefined,
+        prescribeUOMFK: op.prescribingUOM ? op.prescribingUOM.id : undefined,
+        prescribeUOMCode: op.prescribingUOM
+          ? op.prescribingUOM.code
+          : undefined,
+        prescribeUOMDisplayValue: op.prescribingUOM
+          ? op.prescribingUOM.name
+          : undefined,
+        drugFrequencyFK: op.medicationFrequency
+          ? op.medicationFrequency.id
+          : undefined,
+        drugFrequencyCode: op.medicationFrequency
+          ? op.medicationFrequency.code
+          : undefined,
+        drugFrequencyDisplayValue: op.medicationFrequency
+          ? op.medicationFrequency.name
+          : undefined,
+        duration: op.duration,
+      }
+    }
+
     const isEdit = !!values.id
     const newPrescriptionInstruction = isEdit
       ? [
@@ -650,8 +688,6 @@ class Medication extends PureComponent {
     }
     setFieldValue('isActive', op.isActive)
     setFieldValue('selectedMedication', op)
-
-    this.setInstruction(newPrescriptionInstruction.length - 1)
 
     if (
       op.inventoryMedication_MedicationPrecaution &&
@@ -767,6 +803,9 @@ class Medication extends PureComponent {
       type: orders.type,
       visitPurposeFK: orders.visitPurposeFK,
       drugCode: orders.type === '5' ? 'MISC' : undefined,
+      selectedMedication: {
+        medicationStock: [],
+      },
     })
   }
 
@@ -1306,6 +1345,9 @@ class Medication extends PureComponent {
                             isDrugMixture: e.target.value,
                             type: orders.type,
                             visitPurposeFK: orders.visitPurposeFK,
+                            selectedMedication: {
+                              medicationStock: [],
+                            },
                           })
 
                           if (e.target.value) {
