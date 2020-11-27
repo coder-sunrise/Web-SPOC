0ecf166e95bfc5aebc01d9020e59424a4075e75f
Liu Yi Feng <liu_yi_feng@medinno.com>
2020-11-27 18:03:09 +0800

    Adjust WIP Report UI column width

--- src/pages/Report/WIPRevenue/Details.js
+++ src/pages/Report/WIPRevenue/Details.js
@@ -1,8 +1,4 @@
 import React, { PureComponent } from 'react'
-import {
-  IntegratedSummary,
-} from '@devexpress/dx-react-grid'
-
 import { ReportDataGrid } from '@/components/_medisys'
 
 class Details extends PureComponent {
@@ -30,10 +26,10 @@ class Details extends PureComponent {
       { name: 'qty', title: 'Qty.' },
       { name: 'packageName', title: 'Package Name' },
       { name: 'expiryDate', title: 'Exp. Date' },
-      { name: 'bfConsumeQuantity', title: 'BF Consumed Qty.' },
-      { name: 'bfRemainingQuantity', title: 'BF Balance Qty.' },
-      { name: 'bfRemainingAmount', title: 'BF Balance Amt.' },
-      { name: 'bfRevenueAmount', title: 'BF Revenue' },
+      { name: 'bfConsumeQuantity', title: 'Bring Forward Consumed Qty.' },
+      { name: 'bfRemainingQuantity', title: 'Bring Forward Balance Qty.' },
+      { name: 'bfRemainingAmount', title: 'Bring Forward Balance Amt.' },
+      { name: 'bfRevenueAmount', title: 'Bring Forward Revenue' },
       { name: 'consumeQuantity', title: 'Consumed Qty.' },
       { name: 'creditNoteQuantity', title: 'CN Qty.' },
       { name: 'creditNoteAmount', title: 'CN Amount' },
@@ -45,24 +41,24 @@ class Details extends PureComponent {
 
     const RevenueDetailsExtensions = [
       { columnName: 'invoiceDate', type: 'date', sortingEnabled: false, width: 110 },
-      { columnName: 'invoiceNo', sortingEnabled: false, wordWrapEnabled: true, width: 100 },
-      { columnName: 'patientAccountNo', sortingEnabled: false, wordWrapEnabled: true, width: 130 },
-      { columnName: 'invoiceItemName', sortingEnabled: false, wordWrapEnabled: true },
+      { columnName: 'invoiceNo', sortingEnabled: false, wordWrapEnabled: true, width: 90 },
+      { columnName: 'patientAccountNo', sortingEnabled: false, wordWrapEnabled: true, width: 100 },
+      { columnName: 'invoiceItemName', sortingEnabled: false, wordWrapEnabled: true, width: 200 },
       { columnName: 'unitPrice', type: 'currency', currency: true, sortingEnabled: false, width: 100 },
       { columnName: 'qty', type: 'number', sortingEnabled: false, width: 80 },
       { columnName: 'packageName', sortingEnabled: false, wordWrapEnabled: true, width: 160 },
       { columnName: 'expiryDate', type: 'date', sortingEnabled: false, width: 100 },
-      { columnName: 'bfConsumeQuantity', type: 'number', sortingEnabled: false, width: 140 },
-      { columnName: 'bfRemainingQuantity', type: 'number', sortingEnabled: false, width: 120 },
-      { columnName: 'bfRemainingAmount', type: 'currency', currency: true, sortingEnabled: false, width: 130 },
-      { columnName: 'bfRevenueAmount', type: 'currency', currency: true, sortingEnabled: false, width: 120 },
-      { columnName: 'consumeQuantity', type: 'number', sortingEnabled: false, width: 110 },
+      { columnName: 'bfConsumeQuantity', type: 'number', sortingEnabled: false, wordWrapEnabled: true, width: 120 },
+      { columnName: 'bfRemainingQuantity', type: 'number', sortingEnabled: false, wordWrapEnabled: true, width: 110 },
+      { columnName: 'bfRemainingAmount', type: 'currency', currency: true, sortingEnabled: false, wordWrapEnabled: true, width: 110 },
+      { columnName: 'bfRevenueAmount', type: 'currency', currency: true, sortingEnabled: false, wordWrapEnabled: true, width: 110 },
+      { columnName: 'consumeQuantity', type: 'number', sortingEnabled: false, wordWrapEnabled: true, width: 80 },
       { columnName: 'creditNoteQuantity', type: 'number', sortingEnabled: false, width: 80 },
-      { columnName: 'creditNoteAmount', type: 'currency', currency: true, sortingEnabled: false, width: 120 },
+      { columnName: 'creditNoteAmount', type: 'currency', currency: true, sortingEnabled: false, width: 110 },
       { columnName: 'expiryQuantity', type: 'number', sortingEnabled: false, width: 80 },
-      { columnName: 'revenueAmount', type: 'currency', currency: true, sortingEnabled: false, width: 120 },
-      { columnName: 'remainingQuantity', type: 'number', sortingEnabled: false, width: 100 },
-      { columnName: 'remainingAmount', type: 'currency', currency: true, sortingEnabled: false, width: 120 },
+      { columnName: 'revenueAmount', type: 'currency', currency: true, sortingEnabled: false, width: 110 },
+      { columnName: 'remainingQuantity', type: 'number', sortingEnabled: false, wordWrapEnabled: true, width: 80 },
+      { columnName: 'remainingAmount', type: 'currency', currency: true, sortingEnabled: false, width: 110 },
     ]
 
     return (
