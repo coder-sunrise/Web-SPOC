fe0843c6222e4d367c4bba687c54a87ced52ddb8
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2021-05-27 17:45:56 +0800

    14066: Service Maintenance Announcement

--- src/components/_medisys/Notification/index.js
+++ src/components/_medisys/Notification/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames'
 // material ui
 import { withStyles, Divider } from '@material-ui/core'
 import Notifications from '@material-ui/icons/Notifications'
 import Refresh from '@material-ui/icons/Refresh'
 // common components
-import { Badge, Popper, Button, Tabs, IconButton } from '@/components'
+import { Badge, Popover, Button, Tabs, IconButton } from '@/components'
 // sub components
 import customDropdownStyle from '@/assets/jss/material-dashboard-pro-react/components/customDropdownStyle'
+import { SystemMessageList } from '@/components/_medisys'
 import NotificationList from './NotificationList'
 // assets
 import { TYPES } from './constants'
@@ -23,6 +24,7 @@ const styles = (theme) => ({
 
 const NotificationComponent = ({
   notifications = [],
+  systemMessage,
   dispatch,
   classes,
   theme,
@@ -33,22 +35,33 @@ const NotificationComponent = ({
     })
   }
 
+  const [
+    showNotification,
+    setShowNotification,
+  ] = useState(false)
+
+  const { totalUnReadCount = 0 } = systemMessage
+
+  const toggleVisibleChange = () => {
+    setShowNotification(!showNotification)
+  }
+
   const overlay = (
     <div style={{ position: 'relative', width: 600 }}>
       <Tabs
         type='line'
-        // style={{ marginTop: 20 }}
-        // activeKey={active}
-        // defaultActivekey='1'
-        // onChange={(e) => setActive(e)}
         options={TYPES.map((o) => {
           const list = notifications.filter((m) => !o.id || m.type === o.id)
+          let unReadCounts =
+            o.id === 4 ? totalUnReadCount : list.filter((m) => !m.read).length
+
           return {
             ...o,
-            name: `${o.name} ${list.filter((m) => !m.read).length > 0
-              ? `(${list.filter((m) => !m.read).length})`
-              : ''}`,
-            content: (
+            name: `${o.name} ${unReadCounts > 0 ? `(${unReadCounts})` : ''}`,
+            content: o.id === 4 ? (
+              <SystemMessageList dispatch={dispatch} type={o.id}
+                setShowNotification={setShowNotification} />
+            ) : (
               <NotificationList
                 notifications={list}
                 dispatch={dispatch}
@@ -68,19 +81,22 @@ const NotificationComponent = ({
   )
 
   return (
-    <Popper
+    <Popover
+      icon={null}
       trigger='click'
       className={classnames({
-        [classes.pooperResponsive]: true,
-        [classes.pooperNav]: true,
+        [ classes.pooperResponsive ]: true,
+        [ classes.pooperNav ]: true,
       })}
-      overlay={overlay}
+      content={overlay}
+      onVisibleChange={toggleVisibleChange}
+      visible={showNotification}
     >
       <Badge
         badgeContent={
           notifications.filter(
             (o) => TYPES.find((t) => !t.id || o.type === t.id) && !o.read,
-          ).length
+          ).length + totalUnReadCount
         }
         color='primary'
         anchorOrigin={{
@@ -88,11 +104,13 @@ const NotificationComponent = ({
           horizontal: 'left',
         }}
       >
-        <Button justIcon color='transparent'>
+        <Button justIcon color='transparent' onClick={() => {
+          setShowNotification(true)
+        }}>
           <Notifications fontSize='large' />
         </Button>
       </Badge>
-    </Popper>
+    </Popover>
   )
 }
 
---
+++ src/components/_medisys/SystemMessage/Content.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react'
+import moment from 'moment'
+// material ui
+import {
+  Button,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  withStyles,
+} from '@material-ui/core'
+import { infoColor } from 'mui-pro-jss'
+import Info from '@material-ui/icons/Info'
+import {
+  GridItem,
+  GridContainer,
+  dateFormatLongWithTimeNoSec,
+} from '@/components'
+import { NOTIFICATION_TYPE } from '@/utils/constants'
+
+const styles = () => ({
+  root: { maxHeight: 100, background: '#f0f8ff' },
+  itemContainer: { display: 'flex' },
+  messageText: {},
+  timestampText: {
+    color: 'grey',
+    fontSize: 12,
+    marginTop: 5,
+  },
+  itemRoot: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingRight: 30,
+    cursor: 'pointer',
+    '&:not(:last-child)': {
+      borderBottom: `1px solid rgba(0, 0, 0, 0.12)`,
+    },
+    '&.Mui-disabled': {
+      opacity: 1,
+      backgroundColor: 'white',
+    },
+  },
+  icon: {
+    minWidth: 38,
+  },
+  titleLinkBtn: {
+    paddingLeft: '0px',
+    paddingTop: '0px !important',
+    textTransform: 'none',
+    textAlign: 'left',
+  },
+  copyBtn: {
+    paddingTop: '0px !important',
+  },
+})
+
+const Content = ({ systemMessage, classes, dispatch, setShowNotification }) => {
+  let messageTitle = systemMessage.title || ''
+
+  const handleDismiss = () => {
+    dispatch({
+      type: 'systemMessage/dismiss',
+      payload: {
+        id: systemMessage.id,
+      },
+    })
+  }
+
+  const handleOpenDetail = () => {
+    dispatch({
+      type: 'systemMessage/updateState',
+      payload: {
+        entity: systemMessage,
+      },
+    })
+
+    dispatch({
+      type: 'global/updateState',
+      payload: {
+        showSystemMessage: true,
+      },
+    })
+
+    setShowNotification(false)
+  }
+
+  return (
+    <ListItem button alignItems='flex-start' className={classes.itemRoot}>
+      <ListItemText
+        primary={
+          <div>
+            <div className={classes.itemContainer}>
+              <Button
+                size='sm'
+                link
+                color='primary'
+                className={classes.titleLinkBtn}
+                onClick={handleOpenDetail}
+                style={{ display: 'block' }}
+              >
+                {messageTitle}
+              </Button>
+            </div>
+          </div>
+        }
+        secondary={
+          <GridContainer>
+            <GridItem
+              md={10}
+              style={{
+                paddingLeft: 0,
+              }}
+            >
+              <React.Fragment>
+                <p className={classes.timestampText}>
+                  {moment(systemMessage.createDate).format(
+                    dateFormatLongWithTimeNoSec,
+                  )}
+                </p>
+              </React.Fragment>
+            </GridItem>
+            {!systemMessage.isRead && !systemMessage.isDismissed ? (
+              <GridItem md={2}>
+                <Button
+                  size='sm'
+                  link
+                  color='primary'
+                  onClick={handleDismiss}
+                  className={classes.copyBtn}
+                >
+                  Dismiss
+                </Button>
+              </GridItem>
+            ) : (
+              ''
+            )}
+          </GridContainer>
+        }
+      />
+    </ListItem>
+  )
+}
+
+export default withStyles(styles, { name: 'Content' })(Content)
