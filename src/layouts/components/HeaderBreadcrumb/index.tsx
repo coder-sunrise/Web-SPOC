d46a76baebf93a236fda4eb9a0001c95b1dc36ec
Yang Feixiang <YangFeiXiang@medinno.onmicrosoft.com>
2021-06-17 10:25:31 +0800

    fix headerbreadcrumb issue

--- src/layouts/components/HeaderBreadcrumb/index.tsx
+++ src/layouts/components/HeaderBreadcrumb/index.tsx
@@ -3,15 +3,30 @@ import { Breadcrumb } from 'antd'
 import React, { useState, useRef } from 'react'
 import { useIntl, Link } from 'umi'
 import { RightOutlined } from '@ant-design/icons'
+import { Theme } from '@material-ui/core/styles/createMuiTheme'
+import { withStyles } from '@material-ui/styles'
 
-import styles from './style.less'
-
-interface HeaderBreadcrumbProps {
+interface IHeaderBreadcrumbProps {
   breadcrumb: any;
+  classes: any;
 }
 
-const HeaderBreadcrumb: React.FC<HeaderBreadcrumbProps> = (props) => {
-  const { breadcrumb } = props
+const styles = (theme: Theme) => ({
+  breadcrumbtext: {
+    fontSize: '18px',
+    color: 'black',
+  },
+  breadcrumblink: {
+    fontSize: '18px',
+    color: 'black',
+    '&:hover': {
+      color: '#4255bd',
+    },
+  },
+})
+
+const HeaderBreadcrumb: React.FC<IHeaderBreadcrumbProps> = (props) => {
+  const { breadcrumb, classes } = props
   const { pathname } = window.location
   const { formatMessage } = useIntl()
 
@@ -19,20 +34,24 @@ const HeaderBreadcrumb: React.FC<HeaderBreadcrumbProps> = (props) => {
     const pathSnippets = pathname.split('/').filter(i => i)
     const extraBreadcrumbItems = pathSnippets.map((_, index) => {
       const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
-      return (
-        <Breadcrumb.Item key={url}>
-          {
-            pathname === url
-              ? <span className={styles.breadcrumbtext}>{breadcrumb[url].name}</span>
-              : (<Link to={url}><span className={styles.breadcrumblink}>{breadcrumb[url].name}</span></Link>)
-          }
-        </Breadcrumb.Item>
-      )
+      const breadcrumbItem = breadcrumb[url]
+      if (breadcrumbItem && breadcrumbItem.name) {
+        return (
+          <Breadcrumb.Item key={url}>
+            {
+              pathname === url
+                ? <span className={classes.breadcrumbtext}>{breadcrumbItem.name}</span>
+                : (<Link to={url}><span className={classes.breadcrumblink}>{breadcrumbItem.name}</span></Link>)
+            }
+          </Breadcrumb.Item>
+        )
+      }
+      return <></>
     })
 
     const breadcrumbItems = [
       <Breadcrumb.Item key="home">
-        <Link to="/"> <span className={styles.breadcrumblink}> {formatMessage({ id: 'menu.home' })}</span></Link>
+        <Link to="/"> <span className={classes.breadcrumblink}> {formatMessage({ id: 'menu.home' })}</span></Link>
       </Breadcrumb.Item>,
     ].concat(extraBreadcrumbItems)
 
@@ -42,4 +61,4 @@ const HeaderBreadcrumb: React.FC<HeaderBreadcrumbProps> = (props) => {
   return <></>
 }
 
-export default HeaderBreadcrumb
+export default withStyles(styles)(HeaderBreadcrumb)
