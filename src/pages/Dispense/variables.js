490568d61197742519596759434d888652964977
yuchen <yuchen@medinno.onmicrosoft.com>
2021-07-26 17:01:31 +0800

    dev

--- src/pages/Dispense/variables.js
+++ src/pages/Dispense/variables.js
@@ -124,18 +124,44 @@ export const PrescriptionColumnExtensions = (
     width: '30%',
     render: row => {
       return (
-        <div style={{ position: 'relative' }}>
-          <div
-            style={{
-              wordWrap: 'break-word',
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {row.name}
-            {row.isPreOrder && <Tooltip title='Pre-Order'><Tag color="#4255bd" style={{ position: 'absolute', top: 0, right: 10, borderRadius: 10 }}>Pre</Tag></Tooltip>}
-            {lowStockIndicator(row, 'inventoryMedicationFK')}
+        <Tooltip
+          title={
+            <div>
+              {`Code/Name: ${row.code} / ${row.name}`}
+              <br />
+              {`UnitPrice/UOM: ${currencySymbol}${numeral(row.unitPrice).format(
+                currencyFormat,
+              )} / ${row.orderUOM}`}
+            </div>
+          }
+        >
+          <div style={{ position: 'relative' }}>
+            <div
+              style={{
+                wordWrap: 'break-word',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {row.name}
+              {row.isPreOrder && (
+                <Tooltip title='Pre-Order'>
+                  <Tag
+                    color='#4255bd'
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 10,
+                      borderRadius: 10,
+                    }}
+                  >
+                    Pre
+                  </Tag>
+                </Tooltip>
+              )}
+              {lowStockIndicator(row, 'inventoryMedicationFK')}
+            </div>
           </div>
-        </div>
+        </Tooltip>
       )
     },
   },
@@ -163,7 +189,17 @@ export const PrescriptionColumnExtensions = (
     columnName: 'totalAfterItemAdjustment',
     width: 110,
     type: 'currency',
-    render: (row) => <NumberInput value={(row.isPreOrder && !row.isChargeToday) ? 0 : row.totalAfterItemAdjustment} text currency />
+    render: row => (
+      <NumberInput
+        value={
+          row.isPreOrder && !row.isChargeToday
+            ? 0
+            : row.totalAfterItemAdjustment
+        }
+        text
+        currency
+      />
+    ),
   },
   {
     columnName: 'adjAmt',
@@ -335,18 +371,44 @@ export const VaccinationColumnExtensions = (
     width: '60%',
     render: row => {
       return (
-        <div style={{ position: 'relative' }}>
-          <div
-            style={{
-              wordWrap: 'break-word',
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {row.name}
-            {row.isPreOrder && <Tooltip title='Pre-Order'><Tag color="#4255bd" style={{ position: 'absolute', top: 0, right: 10, borderRadius: 10 }}>Pre</Tag></Tooltip>}
-            {lowStockIndicator(row, 'inventoryVaccinationFK')}
+        <Tooltip
+          title={
+            <div>
+              {`Code/Name: ${row.code} / ${row.name}`}
+              <br />
+              {`UnitPrice/UOM: ${currencySymbol}${numeral(row.unitPrice).format(
+                currencyFormat,
+              )} / ${row.dispenseUOM}`}
+            </div>
+          }
+        >
+          <div style={{ position: 'relative' }}>
+            <div
+              style={{
+                wordWrap: 'break-word',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {row.name}
+              {row.isPreOrder && (
+                <Tooltip title='Pre-Order'>
+                  <Tag
+                    color='#4255bd'
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 10,
+                      borderRadius: 10,
+                    }}
+                  >
+                    Pre
+                  </Tag>
+                </Tooltip>
+              )}
+              {lowStockIndicator(row, 'inventoryVaccinationFK')}
+            </div>
           </div>
-        </div>
+        </Tooltip>
       )
     },
   },
@@ -374,7 +436,17 @@ export const VaccinationColumnExtensions = (
     columnName: 'totalAfterItemAdjustment',
     width: 110,
     type: 'currency',
-    render: (row) => <NumberInput value={(row.isPreOrder && !row.isChargeToday) ? 0 : row.totalAfterItemAdjustment} text currency />
+    render: row => (
+      <NumberInput
+        value={
+          row.isPreOrder && !row.isChargeToday
+            ? 0
+            : row.totalAfterItemAdjustment
+        }
+        text
+        currency
+      />
+    ),
   },
   {
     columnName: 'adjAmt',
@@ -501,14 +573,28 @@ export const OtherOrdersColumnExtensions = (viewOnly = false, onPrint) => [
     render: row => {
       return (
         <div style={{ position: 'relative' }}>
-        <div
-          style={{
-            wordWrap: 'break-word',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {row.type}
-            {row.isPreOrder && <Tooltip title='Pre-Order'><Tag color="#4255bd" style={{ position: 'absolute', top: 0, right: -10, borderRadius: 10 }}>Pre</Tag></Tooltip>}
+          <div
+            style={{
+              wordWrap: 'break-word',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {row.type}
+            {row.isPreOrder && (
+              <Tooltip title='Pre-Order'>
+                <Tag
+                  color='#4255bd'
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    right: -10,
+                    borderRadius: 10,
+                  }}
+                >
+                  Pre
+                </Tag>
+              </Tooltip>
+            )}
           </div>
         </div>
       )
@@ -520,11 +606,18 @@ export const OtherOrdersColumnExtensions = (viewOnly = false, onPrint) => [
     width: '60%',
     render: row => {
       const { code = '', description = '', unitPrice = 0 } = row
-      const title = `${description} - ${code} (${currencySymbol}${numeral(
-        unitPrice,
-      ).format(currencyFormat)})`
       return (
-        <Tooltip title={title}>
+        <Tooltip
+          title={
+            <div>
+              {`Code/Name: ${code} / ${description}`}
+              <br />
+              {`UnitPrice/UOM: ${currencySymbol}${numeral(unitPrice).format(
+                currencyFormat,
+              )} / ${row.dispenseUOM || '-'}`}
+            </div>
+          }
+        >
           <div style={{ position: 'relative' }}>
             <div
               style={{
@@ -597,7 +690,11 @@ export const OtherOrdersColumnExtensions = (viewOnly = false, onPrint) => [
           text
           currency
           showZero
-          value={(row.isPreOrder && !row.isChargeToday) ? 0 : row.totalAfterItemAdjustment}
+          value={
+            row.isPreOrder && !row.isChargeToday
+              ? 0
+              : row.totalAfterItemAdjustment
+          }
         />
       )
     },
@@ -823,11 +920,18 @@ export const PackageColumnExtensions = onPrint => [
     width: '60%',
     render: row => {
       const { code = '', description = '', unitPrice = 0 } = row
-      const title = `${description} - ${code} (${currencySymbol}${numeral(
-        unitPrice,
-      ).format(currencyFormat)})`
       return (
-        <Tooltip title={title}>
+        <Tooltip
+          title={
+            <div>
+              {`Code/Name: ${code} / ${description}`}
+              <br />
+              {`UnitPrice/UOM: ${currencySymbol}${numeral(unitPrice).format(
+                currencyFormat,
+              )} / ${row.dispenseUOM || '-'}`}
+            </div>
+          }
+        >
           <div
             style={{
               wordWrap: 'break-word',
