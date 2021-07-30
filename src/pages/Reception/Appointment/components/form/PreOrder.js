13f2ebf43e476e8265b76710968c2338c93e4f05
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2021-07-30 13:15:03 +0800

    fix pre order bug:19171, 19166, 19147, 19191, 19168, 19173

--- src/pages/Reception/Appointment/components/form/PreOrder.js
+++ src/pages/Reception/Appointment/components/form/PreOrder.js
@@ -13,22 +13,27 @@ import { InventoryTypes } from '@/utils/codes'
 import { futureApptTableParams, previousApptTableParams } from './variables'
 import { Delete } from '@material-ui/icons'
 
-const PreOrder = ({ values, deletePreOrderItem }) => {
+const PreOrder = ({ values, deletePreOrderItem, disabled }) => {
   const { currentAppointment: { appointmentPreOrderItem = [] } } = values
   return <div>
     <div style={{ margin: '6px 0px' }}>Pre-Order Actualization</div>
     <CommonTableGrid
       forceRender
       size='sm'
-      rows={appointmentPreOrderItem.filter(x => !x.isDeleted)}
+      rows={appointmentPreOrderItem.filter(x => !x.isDeleted).map(appt => {
+        return {
+          ...appt,
+          disabled
+        }
+      })}
       FuncProps={{ pager: false }}
       getRowId={(row) => row.actualizedPreOrderItemFK}
       columns={[
-        { name: 'preOrderItemType', title: 'Category' },
+        { name: 'preOrderItemType', title: 'Type' },
         { name: 'itemName', title: 'Name' },
-        { name: 'quantity', title: 'Quantity' },
+        { name: 'quantity', title: 'Order Qty.' },
         { name: 'orderByUser', title: 'Order By' },
-        { name: 'orderDate', title: 'Order Date & Time' },
+        { name: 'orderDate', title: 'Order Date' },
         { name: 'remarks', title: 'Remarks' },
         { name: 'amount', title: 'Amount' },
         { name: 'hasPaid', title: 'Paid' },
@@ -48,7 +53,7 @@ const PreOrder = ({ values, deletePreOrderItem }) => {
           },
         },
         { columnName: 'orderByUser', sortingEnabled: false },
-        { columnName: 'orderDate', sortingEnabled: false, type: 'date', showTime: true, width: 180 },
+        { columnName: 'orderDate', sortingEnabled: false, type: 'date', width: 140, render: (row) => <span>{moment(row.orderDate).format('DD MMM YYYY HH:mm')}</span> },
         { columnName: 'remarks', sortingEnabled: false },
         { columnName: 'amount', sortingEnabled: false, type: 'currency', width: 90 },
         { columnName: 'hasPaid', sortingEnabled: false, width: 50, render: (row) => row.hasPaid ? 'Yes' : 'No' },
@@ -56,15 +61,16 @@ const PreOrder = ({ values, deletePreOrderItem }) => {
           columnName: 'action', width: 60, render: (row) => {
             return <Authorized authority='appointment.actualizepreorder'>
               <Button size='sm'
-              justIcon
-              color='danger'
-              onClick={() => {
-                if (deletePreOrderItem) {
-                  deletePreOrderItem(row.actualizedPreOrderItemFK)
-                }
-              }}>
-              <Delete />
-            </Button>
+                justIcon
+                color='danger'
+                disabled={row.disabled}
+                onClick={() => {
+                  if (deletePreOrderItem) {
+                    deletePreOrderItem(row.actualizedPreOrderItemFK)
+                  }
+                }}>
+                <Delete />
+              </Button>
             </Authorized>
           }
         },
