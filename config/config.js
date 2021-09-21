1f4812352b21c38f7f24de847d261be6b975c764
Shiyuan <li_shi_yuan@medinno.com>
2021-09-21 15:15:52 +0800

    revert

--- config/config.js
+++ config/config.js
@@ -67,7 +67,8 @@ export default defineConfig({
     'process.env.client_env': 'development',
     'process.env.client_secret':
       '20e392d2ea9bfa76f2a9cb26c31a34d675ad81281a31f89ed5d572de8da0b9e7',
-    'process.env.url': 'https://medicloud-uat-api-200729.semr2.com',
+    'process.env.url': 'http://localhost:55314',
+    //'process.env.url': 'https://medicloud-uat-api-200729.semr2.com',
     'process.env.signalrUrl':
       'https://semr2dev2010-websocket.emr.com.sg/notificationHub',
   },
