3149cf856f2d38653bc250d9268ef9f23c6f6090
mgblackwater <mgblackwater@gmail.com>
2021-10-06 13:25:54 +0800

    Add assignment and combined order init features

--- src/pages/Radiology/Worklist/services/index.js
+++ src/pages/Radiology/Worklist/services/index.js
@@ -6,5 +6,6 @@ const fns = {
   query: params => service.query(url, params),
   queryList: params => service.queryList(url, params),
   upsert: params => service.upsert(url, params),
+  cancel: params => service.upsert(url + '/cancel', params),
 }
 export default fns
