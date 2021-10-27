2e1ef7db1ace76645ed6b9f5d07983957dcdd467
mgblackwater <mgblackwater@gmail.com>
2021-10-27 14:10:39 +0800

    Radiology Worklist - Add info icon near to Filter Date

--- src/locales/en-US/radiology.js
+++ src/locales/en-US/radiology.js
@@ -3,6 +3,6 @@ export default {
     'Patient Name, Acc No., Patient Ref. No., Accession No.',
   'radiology.search.dateFrom': 'From',
   'radiology.search.dateTo': 'To',
-  'radiology.search.myPatientOnly': 'My Patient Only',
+  'radiology.search.myPatientOnly': 'My Patient',
   'radiology.search.urgentOnly': 'Urgent',
 }
