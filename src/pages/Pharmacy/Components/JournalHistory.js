65537f864789046104edb230f7dec99bd806ac31
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2021-11-02 10:50:54 +0800

    pharmacy journal history

--- src/pages/Pharmacy/Components/JournalHistory.js
+++ src/pages/Pharmacy/Components/JournalHistory.js
@@ -1,66 +1,77 @@
 import { Steps } from 'antd'
 import _ from 'lodash'
 import moment from 'moment'
-import { pharmacyStatus } from '@/utils/codes'
-import { Tooltip } from '@/components'
+import { Tooltip, Button, SizeContainer } from '@/components'
+import { Divider } from '@material-ui/core'
+import Close from '@material-ui/icons/Close'
+import styles from './PharmacyStep.less'
 const { Step } = Steps
 
-export const JournalHistory = ({ statusHistory = [] }) => {
-  const orderStatusHistory = _.orderBy(
-    statusHistory,
+export const JournalHistory = ({ journalHistoryList = [], onClose }) => {
+  const orderJournalHistoryList = _.orderBy(
+    journalHistoryList,
     ['actionDate'],
-    ['statusFK'],
     ['desc'],
-  ).map(item => {
-    return {
-      ...item,
-      name: pharmacyStatus.find(s => s.statusFK === item.statusFK).name,
-    }
-  })
+  )
   return (
-    <div style={{ width: 400, padding: '10px 6px' }}>
-      <div style={{ fontSize: '1.2rem', marginBottom: 10 }}>
-        Journal History
-      </div>
-      <Steps
-        size='small'
-        direction='vertical'
-        current={orderStatusHistory.length - 1}
-      >
-        {orderStatusHistory.map((status, index) => {
-          return (
-            <Step
-              title={<span style={{ fontWeight: 600 }}>{status.name}</span>}
-              subTitle={`${
-                status.actionByUserTitle &&
-                status.actionByUserTitle.trim().length
-                  ? `${status.actionByUserTitle}. `
-                  : ''
-              }${status.actionByUser || ''}`}
-              description={
-                <div>
-                  {`${moment(status.actionDate).format('DD MMM YYYY HH:mm')}`}
-                </div>
-              }
-              icon={
-                <div
-                  style={{
-                    width: 24,
-                    height: 24,
-                    borderRadius: 12,
-                    backgroundColor: '#33CC33',
-                    textAlign: 'center',
-                    fontSize: '0.7rem',
-                    color: 'white',
-                  }}
-                >
-                  {index}
-                </div>
-              }
-            ></Step>
-          )
-        })}
-      </Steps>
+    <div style={{ width: 350, padding: '10px' }}>
+      <h4 style={{ display: 'inline-block' }}>Journal History</h4>
+      <Tooltip title='Close Journal History'>
+        <Button
+          justIcon
+          style={{ float: 'right', right: '-8px', top: 5 }}
+          key='close'
+          authority='none'
+          aria-label='Close'
+          color='transparent'
+          onClick={onClose}
+        >
+          <Close />
+        </Button>
+      </Tooltip>
+      <SizeContainer size='sm'>
+        <Divider style={{ marginBottom: 8 }} />
+        <Steps
+          className={styles.journalHistory}
+          size='small'
+          direction='vertical'
+          //current={orderJournalHistoryList.length - 1}
+        >
+          {orderJournalHistoryList.map(item => {
+            return (
+              <Step
+                status='finish'
+                title={
+                  <div>
+                    <p style={{ fontWeight: 600, height: 16 }}>
+                      {`${moment(item.actionDate).format('DD MMM YYYY HH:mm')}`}
+                    </p>
+                    <p style={{ fontWeight: 400, height: 16 }}>
+                      {item.actionDescription}
+                    </p>
+                  </div>
+                }
+                description={`by ${
+                  item.actionByUserTitle && item.actionByUserTitle.trim().length
+                    ? `${item.actionByUserTitle}. `
+                    : ''
+                }${item.actionByUserName || ''}`}
+                icon={
+                  <div
+                    style={{
+                      width: 20,
+                      height: 20,
+                      borderRadius: 10,
+                      border: '2px solid #2A55FF',
+                      margin: 1,
+                    }}
+                  />
+                }
+              ></Step>
+            )
+          })}
+        </Steps>
+      </SizeContainer>
     </div>
   )
 }
