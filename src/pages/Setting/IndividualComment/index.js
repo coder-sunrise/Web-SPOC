1049f2e14def86362a2a8d1d98965de052019d4f
Shiyuan <li_shi_yuan@medinno.com>
2022-01-05 17:13:31 +0800

    comment setting

---
+++ src/pages/Setting/IndividualComment/index.js
@@ -0,0 +1,87 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'dva'
+import $ from 'jquery'
+import { withStyles } from '@material-ui/core'
+import basicStyle from 'mui-pro-jss/material-dashboard-pro-react/layouts/basicLayout'
+
+import {
+  CardContainer,
+  CommonModal,
+  withSettingBase,
+  CodeSelect,
+} from '@/components'
+
+import Filter from './Filter'
+import Grid from './Grid'
+import Detail from './Detail'
+
+const styles = theme => ({
+  ...basicStyle(theme),
+})
+
+@connect(({ settingIndividualComment, global, clinicSettings }) => ({
+  settingIndividualComment,
+  mainDivHeight: global.mainDivHeight,
+  clinicSettings: clinicSettings.settings || clinicSettings.default,
+}))
+@withSettingBase({ modelName: 'settingIndividualComment' })
+class IndividualComment extends PureComponent {
+  state = {}
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch({
+      type: 'codetable/fetchCodes',
+      payload: {
+        code: 'ctexaminationitem',
+        force: true,
+      },
+    }).then(() => {
+      dispatch({
+        type: 'settingIndividualComment/query',
+      })
+    })
+  }
+
+  toggleModal = () => {
+    this.props.dispatch({
+      type: 'settingIndividualComment/updateState',
+      payload: {
+        showModal: !this.props.settingIndividualComment.showModal,
+      },
+    })
+  }
+
+  render() {
+    const { settingIndividualComment, mainDivHeight = 700 } = this.props
+    const cfg = {
+      toggleModal: this.toggleModal,
+    }
+    let height = mainDivHeight - 110 - ($('.filterBar').height() || 0)
+    if (height < 300) height = 300
+    return (
+      <CardContainer hideHeader>
+        <div className='filterBar'>
+          <Filter {...cfg} {...this.props} />
+        </div>
+        <Grid {...cfg} {...this.props} height={height} />
+        <CommonModal
+          open={settingIndividualComment.showModal}
+          observe='IndividualCommentDetail'
+          title={
+            settingIndividualComment.entity
+              ? 'Edit Medical Check Up Individual Comment'
+              : 'Add Medical Check Up Individual Comment'
+          }
+          maxWidth='md'
+          bodyNoPadding
+          onClose={this.toggleModal}
+          onConfirm={this.toggleModal}
+        >
+          <Detail {...cfg} {...this.props} />
+        </CommonModal>
+      </CardContainer>
+    )
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(IndividualComment)
---
+++ src/pages/Setting/SummaryComment/index.js
@@ -0,0 +1,87 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'dva'
+import $ from 'jquery'
+import { withStyles } from '@material-ui/core'
+import basicStyle from 'mui-pro-jss/material-dashboard-pro-react/layouts/basicLayout'
+
+import {
+  CardContainer,
+  CommonModal,
+  withSettingBase,
+  CodeSelect,
+} from '@/components'
+
+import Filter from './Filter'
+import Grid from './Grid'
+import Detail from './Detail'
+
+const styles = theme => ({
+  ...basicStyle(theme),
+})
+
+@connect(({ settingSummaryComment, global, clinicSettings }) => ({
+  settingSummaryComment,
+  mainDivHeight: global.mainDivHeight,
+  clinicSettings: clinicSettings.settings || clinicSettings.default,
+}))
+@withSettingBase({ modelName: 'settingSummaryComment' })
+class SummaryComment extends PureComponent {
+  state = {}
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch({
+      type: 'codetable/fetchCodes',
+      payload: {
+        code: 'ctexaminationitem',
+        force: true,
+      },
+    }).then(() => {
+      dispatch({
+        type: 'settingSummaryComment/query',
+      })
+    })
+  }
+
+  toggleModal = () => {
+    this.props.dispatch({
+      type: 'settingSummaryComment/updateState',
+      payload: {
+        showModal: !this.props.settingSummaryComment.showModal,
+      },
+    })
+  }
+
+  render() {
+    const { settingSummaryComment, mainDivHeight = 700 } = this.props
+    const cfg = {
+      toggleModal: this.toggleModal,
+    }
+    let height = mainDivHeight - 110 - ($('.filterBar').height() || 0)
+    if (height < 300) height = 300
+    return (
+      <CardContainer hideHeader>
+        <div className='filterBar'>
+          <Filter {...cfg} {...this.props} />
+        </div>
+        <Grid {...cfg} {...this.props} height={height} />
+        <CommonModal
+          open={settingSummaryComment.showModal}
+          observe='SummaryCommentDetail'
+          title={
+            settingSummaryComment.entity
+              ? 'Edit Medical Check Up Summary Comment'
+              : 'Add Medical Check Up Summary Comment'
+          }
+          maxWidth='md'
+          bodyNoPadding
+          onClose={this.toggleModal}
+          onConfirm={this.toggleModal}
+        >
+          <Detail {...cfg} {...this.props} />
+        </CommonModal>
+      </CardContainer>
+    )
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(SummaryComment)
