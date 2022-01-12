e0e14ac49e3afeaa254d42bfb692d8bf7f65ff1b
Zhong Yang <ZhongYang@medinno.onmicrosoft.com>
2022-01-12 21:24:49 +0800

    show Jpn for ICD10 Diagnosis in diagnosis Trending Report

--- src/components/_medisys/ICD10DiagnosisSelect/index.js
+++ src/components/_medisys/ICD10DiagnosisSelect/index.js
@@ -15,41 +15,45 @@ const ICD10DiagnosisSelect = ({
   label,
   filterStyle = { position: 'absolute', bottom: 8, right: 0 },
   clinicSettings,
-  diagnosis,
   from = 'report',
   selectDiagnosisCode,
   favouriteDiagnosis,
   handelSaveDiagnosisAsFavourite,
+  defaultLanguage,
+  onICD10AMLanguageChange = () => {},
   ...props
 }) => {
-
   const accessRight = Authorized.check('queue.consultation.widgets.diagnosis')
 
   let selectProps = props
-  const initMaxTagCount = props.field && props.field.value && props.field.value.length === 1 ? 1 : 0
-  const [ maxTagCount, setMaxTagCount ] = useState(
-    props.maxTagCount !== undefined ? props.maxTagCount : initMaxTagCount
+  const initMaxTagCount =
+    props.field && props.field.value && props.field.value.length === 1 ? 1 : 0
+  const [maxTagCount, setMaxTagCount] = useState(
+    props.maxTagCount !== undefined ? props.maxTagCount : initMaxTagCount,
   )
-  if (props.maxTagCount === undefined && props.mode && props.mode === 'multiple') {
+  if (
+    props.maxTagCount === undefined &&
+    props.mode &&
+    props.mode === 'multiple'
+  ) {
     selectProps = { ...props, maxTagCount }
   }
 
-  const [ ctICD10Diagnosis, setICD10Diagnosis ] = useState([])
-  const [ currentDiagnosisLanguage, setcurrentDiagnosisLanguage ] = useState(diagnosis.favouriteDiagnosisLanguage)
-  const [ labelValue, setLabelValue ] = useState()
-
-  useEffect(
-    () => {
-      setcurrentDiagnosisLanguage(diagnosis.favouriteDiagnosisLanguage)
-      setLabelValue(diagnosis.favouriteDiagnosisLanguage === 'EN' ? 'displayvalue' : 'JpnDisplayValue')
-    },
-    [ diagnosis.favouriteDiagnosisLanguage ]
+  const [ctICD10Diagnosis, setICD10Diagnosis] = useState([])
+  const [currentDiagnosisLanguage, setcurrentDiagnosisLanguage] = useState(
+    defaultLanguage,
   )
+  const [labelValue, setLabelValue] = useState()
 
-  const onICD10DiagnosisSearch = async (v) => {
+  useEffect(() => {
+    setcurrentDiagnosisLanguage(defaultLanguage)
+    setLabelValue(defaultLanguage === 'EN' ? 'displayvalue' : 'JpnDisplayValue')
+  }, [defaultLanguage])
+
+  const onICD10DiagnosisSearch = async v => {
     const search = {
       props: 'id,displayvalue,code,JpnDisplayValue',
-      sorting: [ { columnName: 'displayvalue', direction: 'asc' } ],
+      sorting: [{ columnName: 'displayvalue', direction: 'asc' }],
       pagesize: 30,
     }
 
@@ -65,7 +69,10 @@ const ICD10DiagnosisSelect = ({
       searchByLanguage: currentDiagnosisLanguage,
     }
 
-    const response = await queryList('/api/codetable/CTICD10AMDiagnosis', search)
+    const response = await queryList(
+      '/api/codetable/CTICD10AMDiagnosis',
+      search,
+    )
     if (response && response.data) {
       setICD10Diagnosis(response.data.data)
 
@@ -79,22 +86,28 @@ const ICD10DiagnosisSelect = ({
     return response
   }
 
-  const onLanguageChange = (e) => {
+  const onLanguageChange = e => {
+    onICD10AMLanguageChange(e.target.value)
     setcurrentDiagnosisLanguage(e.target.value)
-    e.target.value === 'EN' ? setLabelValue('displayvalue') : setLabelValue('JpnDisplayValue')
+    e.target.value === 'EN'
+      ? setLabelValue('displayvalue')
+      : setLabelValue('JpnDisplayValue')
   }
 
-  const languageOptions = [ { label: ' EN ', value: 'EN' }, { label: ' JP ', value: 'JP' } ]
+  const languageOptions = [
+    { label: ' EN ', value: 'EN' },
+    { label: ' JP ', value: 'JP' },
+  ]
 
   return (
     <div style={{ position: 'relative' }}>
       <Select
         label={label || 'Diagnosis'}
         options={ctICD10Diagnosis}
-        valueField="id"
+        valueField='id'
         labelField={labelValue}
         handleFilter={(input, opt) => true}
-        renderDropdown={(option) => {
+        renderDropdown={option => {
           const { code } = option
           return (
             <div style={{ display: 'flex' }}>
@@ -113,8 +126,10 @@ const ICD10DiagnosisSelect = ({
                 )}
               </div>
               {from === 'Consultaion' && (
-                <div style={{ marginLeft: 'auto', marginRight: -2, height: 20 }}>
-                  {favouriteDiagnosis.find((d) => d === code) ? (
+                <div
+                  style={{ marginLeft: 'auto', marginRight: -2, height: 20 }}
+                >
+                  {favouriteDiagnosis.find(d => d === code) ? (
                     <Star
                       style={{
                         color: '#FFCC00',
@@ -132,7 +147,11 @@ const ICD10DiagnosisSelect = ({
         }}
         query={onICD10DiagnosisSearch}
         onChange={(values, opts) => {
-          if (props.maxTagCount === undefined && props.mode && props.mode === 'multiple') {
+          if (
+            props.maxTagCount === undefined &&
+            props.mode &&
+            props.mode === 'multiple'
+          ) {
             setMaxTagCount(values && values.length === 1 ? 1 : 0)
           }
           if (props.onChange) {
@@ -141,11 +160,18 @@ const ICD10DiagnosisSelect = ({
         }}
         {...selectProps}
       />
-      {from === 'Consultaion' &&
-      selectDiagnosisCode && (
-        <div style={{ ...filterStyle, height: 28, top: 20, bottom: 0, right: -40 }}>
-          {favouriteDiagnosis.find((d) => d === selectDiagnosisCode) ? (
-            <Tooltip title="Click to remove favourite">
+      {from === 'Consultaion' && selectDiagnosisCode && (
+        <div
+          style={{
+            position: 'absolute',
+            height: 28,
+            top: 20,
+            bottom: 0,
+            right: -30,
+          }}
+        >
+          {favouriteDiagnosis.find(d => d === selectDiagnosisCode) ? (
+            <Tooltip title='Click to remove favourite'>
               <Star
                 style={{
                   color: '#FFCC00',
@@ -153,35 +179,52 @@ const ICD10DiagnosisSelect = ({
                   height: 28,
                   cursor: 'pointer',
                 }}
-                onClick={accessRight.rights === 'enable' ? handelSaveDiagnosisAsFavourite : null}
+                onClick={
+                  accessRight.rights === 'enable'
+                    ? handelSaveDiagnosisAsFavourite
+                    : null
+                }
               />
             </Tooltip>
           ) : (
-            <Tooltip title="Click to add to favourite">
+            <Tooltip title='Click to add to favourite'>
               <StarBorder
-                style={{ color: 'gray', width: 28, height: 28, cursor: 'pointer' }}
-                onClick={accessRight.rights === 'enable' ? handelSaveDiagnosisAsFavourite : null}
+                style={{
+                  color: 'gray',
+                  width: 28,
+                  height: 28,
+                  cursor: 'pointer',
+                }}
+                onClick={
+                  accessRight.rights === 'enable'
+                    ? handelSaveDiagnosisAsFavourite
+                    : null
+                }
               />
             </Tooltip>
           )}
         </div>
       )}
       {clinicSettings.isEnableJapaneseICD10Diagnosis === true && (
-        <RadioGroup
-          label='Diagnosis Language'
-          style={{ ...filterStyle, left: 410 }}
-          options={languageOptions}
-          onChange={onLanguageChange}
-          value={currentDiagnosisLanguage}
-        />
+        <div
+          style={{
+            ...filterStyle,
+          }}
+        >
+          <RadioGroup
+            label='Diagnosis Language'
+            options={languageOptions}
+            onChange={onLanguageChange}
+            value={currentDiagnosisLanguage}
+          />
+        </div>
       )}
     </div>
   )
 }
 
-const Connected = connect(({ clinicSettings, diagnosis }) => ({
+const Connected = connect(({ clinicSettings }) => ({
   clinicSettings: clinicSettings.settings,
-  diagnosis,
 }))(ICD10DiagnosisSelect)
 
 export default withStyles({ withTheme: true })(Connected)
