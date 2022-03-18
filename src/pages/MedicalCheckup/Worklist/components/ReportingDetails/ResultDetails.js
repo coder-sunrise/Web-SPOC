2b03f5271428008bd49a8c82d07f61f655c6cf51
Shiyuan <li_shi_yuan@medinno.com>
2022-03-18 14:24:27 +0800

    MC Result Details

--- src/pages/MedicalCheckup/Worklist/components/ReportingDetails/ResultDetails.js
+++ src/pages/MedicalCheckup/Worklist/components/ReportingDetails/ResultDetails.js
@@ -1,9 +1,134 @@
+import React, { useState, useEffect } from 'react'
+import { List } from 'antd'
+import { useDispatch } from 'dva'
+import ReactHtmlParser from 'react-html-parser'
+import { withStyles } from '@material-ui/core'
+
+const styles = () => ({
+  '.ant-list-header': {
+    padding: '5px',
+  },
+})
+
 const ResultDetails = props => {
-  const { height } = props
+  const { visitId, showResultDetails } = props
+  const [data, setData] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (!loaded && showResultDetails && visitId) {
+      setLoaded(true)
+      dispatch({
+        type: 'workitem/getResultDetails',
+        payload: { visitId: visitId },
+      }).then(data => {
+        setData(data)
+      })
+    }
+  }, [showResultDetails])
   return (
-    <div style={{ backgroundColor: 'red', height: height, overflow: 'auto' }}>
-      Result Details
+    <div style={{ height: '100%', overflow: 'auto' }}>
+      {data?.labResultDetails && (
+        <List
+          itemLayout='horizontal'
+          header={<h4 style={{ fontWeight: 'bold' }}>Lab Test</h4>}
+          className='resultDetails'
+          size='small'
+          dataSource={data.labResultDetails}
+          renderItem={item => (
+            <List.Item style={{ padding: 0 }}>
+              <List.Item.Meta
+                style={{ padding: '4px 8px' }}
+                title={
+                  <div>
+                    <h5
+                      style={{
+                        fontWeight: 'bold',
+                        color: '#4355be',
+                        fontSize: '1.05em',
+                      }}
+                    >
+                      {item.testPanel}
+                    </h5>
+                    <div>{`Last Verified By: ${item.verifiedBy}`}</div>
+                  </div>
+                }
+                description={
+                  <div style={{ paddingLeft: 10 }}>
+                    <div style={{ color: 'black' }}>
+                      <div style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
+                        Internal Remarks:{' '}
+                      </div>
+                      <div style={{ display: 'inline-block' }}>
+                        {item.internalRemarks}
+                      </div>
+                    </div>
+                    <div style={{ color: 'black' }}>
+                      <div style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
+                        Report Remarks:
+                      </div>
+                      <span style={{ display: 'inline-block' }}>
+                        {item.reportRemarks}
+                      </span>
+                    </div>
+                  </div>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      )}
+      {data?.radiologyResultDetails && (
+        <List
+          itemLayout='horizontal'
+          header={<h4 style={{ fontWeight: 'bold' }}>Examination</h4>}
+          className='resultDetails'
+          size='small'
+          dataSource={data.radiologyResultDetails}
+          renderItem={item => (
+            <List.Item style={{ padding: 0 }}>
+              <List.Item.Meta
+                style={{ padding: '4px 8px' }}
+                title={
+                  <div>
+                    <h5
+                      style={{
+                        fontWeight: 'bold',
+                        fontSize: '1.05em',
+                        color: '#4355be',
+                      }}
+                    >
+                      {item.serviceName}
+                    </h5>
+                    <div>{`Radiology Technologist: ${item.radiologyTechnologist}`}</div>
+                  </div>
+                }
+                description={
+                  <div style={{ paddingLeft: 10 }}>
+                    <div style={{ color: 'black' }}>
+                      <div style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
+                        Findings:{' '}
+                      </div>
+                      <div style={{ display: 'inline-block' }}>
+                        {ReactHtmlParser(item.findings)}
+                      </div>
+                    </div>
+                    <div style={{ color: 'black' }}>
+                      <div style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
+                        Examination Comments:
+                      </div>
+                      <span style={{ display: 'inline-block' }}>
+                        {item.examinationComments}
+                      </span>
+                    </div>
+                  </div>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      )}
     </div>
   )
 }
-export default ResultDetails
+export default withStyles(styles, { name: 'ResultDetails' })(ResultDetails)
