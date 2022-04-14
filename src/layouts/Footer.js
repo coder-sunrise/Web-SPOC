950dc0f81723cbdfcda45d18ef7e02d964733291
Sun <sun_wang@medinno.onmicrosoft.com>
2022-04-14 10:16:42 +0800

    test

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import { Layout, Icon } from 'antd'
 import GlobalFooter from '@/components/GlobalFooter'
-
+//
 const { Footer } = Layout
 const FooterView = () => (
   <Footer style={{ padding: 0 }}>
