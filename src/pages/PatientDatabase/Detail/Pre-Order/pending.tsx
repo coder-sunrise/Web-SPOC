ad8da72f7de3c29edbd4720e8140f36f4bfe3b2d
Sun <sun_wang@medinno.onmicrosoft.com>
2022-04-22 13:43:29 +0800

    22988

--- src/pages/PatientDatabase/Detail/Pre-Order/pending.tsx
+++ src/pages/PatientDatabase/Detail/Pre-Order/pending.tsx
@@ -253,7 +253,7 @@ const PendingPreOrder: React.FC = (props: any) => {
     const { row, option } = e
     const { dispensingUOM = {}, prescribingUOM = {}, uom = {} } = option
     row.itemName = option?.combinDisplayValue
-    row.code = option?.combinDisplayValue
+    row.code = option?.code
     if (row.preOrderItemType === preOrderItemCategory[0].value) {
       row.preOrderMedicationItem = {
         InventoryMedicationFK: option?.id,
