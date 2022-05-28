bc97e02e5dfa458799fd945a0308361fbf34b82b
Luo Ren Chuan <LuoRenChuan@medinno.onmicrosoft.com>
2022-05-28 18:07:43 +0800

    fix bug 21864

--- src/components/_medisys/Forms/CommonForm/index.js
+++ src/components/_medisys/Forms/CommonForm/index.js
@@ -86,7 +86,9 @@ class CommonForm extends PureComponent {
     let isSignatured = false
     console.log('selection',e.source.documentEditor.selection)
     if(isSelectionInEditRegion) {
-      const { start, end } = e.source.documentEditor.selection
+      const { start, end, documentHelper } = e.source.documentEditor.selection
+      documentHelper.editableDiv.contentEditable = false
+      documentHelper.editableDiv.contenteditable = false
       if(start.currentWidget.children.some(x=> {
         console.log({currentWidget:x,className:x.constructor.name})
         return x.constructor.name === 'ImageElementBox'
