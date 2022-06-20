0c792114bb4db1c632049711f08f5789b002a8fd
Shiyuan <li_shi_yuan@medinno.com>
2022-06-20 20:29:53 +0800

    copayer document order by update date desendingly by default

--- src/pages/Widgets/AttachmentDocument/services/coPayerAttachment.js
+++ src/pages/Widgets/AttachmentDocument/services/coPayerAttachment.js
@@ -3,9 +3,9 @@ import * as service from '@/services/common'
 const url = '/api/CoPayerAttachment'
 
 export const queryList = params =>
-  service.queryList(url, {
-    ...params,
-    sorting: [{ columnName: 'sortOrder', direction: 'asc' }],
-  })
+         service.queryList(url, {
+           ...params,
+           sorting: [{ columnName: 'updateDate', direction: 'asc' }],
+         })
 export const upsert = params => service.upsert(url, params)
 export const remove = params => service.remove(url, params)
