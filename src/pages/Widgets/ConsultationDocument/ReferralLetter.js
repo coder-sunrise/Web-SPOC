751d8f46e5f277731bf643fb26862049da0228e1
Luo Ren Chuan <LuoRenChuan@medinno.onmicrosoft.com>
2022-12-01 15:47:36 +0800

    #24816

--- src/pages/Widgets/ConsultationDocument/ReferralLetter.js
+++ src/pages/Widgets/ConsultationDocument/ReferralLetter.js
@@ -1,99 +1,42 @@
-import React, { Component, PureComponent } from 'react'
-
+import React from 'react'
+import { FastField, withFormik, Field } from 'formik'
+import { compose } from 'redux'
+import moment from 'moment'
 import Yup from '@/utils/yup'
-
 import {
-  Button,
   GridContainer,
   GridItem,
   TextField,
-  notification,
-  Select,
-  CodeSelect,
   DatePicker,
+  MultipleTextField,
   RadioGroup,
-  ProgressButton,
-  CardContainer,
-  confirm,
-  Checkbox,
-  SizeContainer,
-  RichEditor,
-  withFormikExtend,
-  FastField,
-  Field,
-  ButtonSelect,
-  ClinicianSelect,
 } from '@/components'
-import { getClinicianProfile } from './utils'
 
-@withFormikExtend({
-  mapPropsToValues: ({ consultationDocument, codetable, visitEntity }) => {
-    const clinicianProfile = getClinicianProfile(codetable, visitEntity)
-    const values = {
-      ...(consultationDocument.entity ||
-        consultationDocument.defaultReferralLetter()),
-      referredByUserFK: clinicianProfile.userProfileFK,
-    }
-
-    return values
-  },
-  validationSchema: Yup.object().shape({
-    referralDate: Yup.date().required(),
-    referredByUserFK: Yup.number().required(),
-    // to: Yup.string().required(),
-    // address: Yup.string().required(),
-    subject: Yup.string().required(),
-    content: Yup.string().required(),
-  }),
-
-  handleSubmit: (values, { props }) => {
-    const { dispatch, onConfirm, getNextSequence } = props
-    const nextSequence = getNextSequence()
-    dispatch({
-      type: 'consultationDocument/upsertRow',
-      payload: {
-        sequence: nextSequence,
-        ...values,
-      },
-    })
-    if (onConfirm) onConfirm()
-  },
-  displayName: 'AddConsultationDocument',
-})
-class ReferralLetter extends PureComponent {
-  state = {
-    editorReferece: {},
-  }
-
-  setEditorReference = ref => {
-    this.setState({ editorReferece: ref })
-    // this.editorReferece = ref
-  }
-
-  render() {
-    const {
-      currentType,
-      footer,
-      handleSubmit,
-      classes,
-      setFieldValue,
-      templateLoader,
-      height,
-    } = this.props
-    return (
-      <div>
+const ReferralLetter = props => {
+  const { footer, handleSubmit } = props
+  return (
+    <div>
+      <div style={{ paddingBottom: 8 }}>
         <GridContainer>
           <GridItem xs={6}>
             <FastField
-              name='referralDate'
+              name='patientName'
               render={args => {
-                return <DatePicker label='Date' autoFocus {...args} />
+                return <TextField label='Name' disabled {...args} />
               }}
             />
           </GridItem>
         </GridContainer>
         <GridContainer>
-          <GridItem xs={6}>
+          <GridItem md={6}>
+            <FastField
+              name='from'
+              render={args => {
+                return <TextField label='From' disabled {...args} />
+              }}
+            />
+          </GridItem>
+          <GridItem md={6}>
             <FastField
               name='to'
               render={args => {
@@ -101,25 +44,9 @@ class ReferralLetter extends PureComponent {
               }}
             />
           </GridItem>
-          <GridItem xs={6}>
-            <FastField
-              name='referredByUserFK'
-              render={args => {
-                return <ClinicianSelect label='From' disabled {...args} />
-              }}
-            />
-          </GridItem>
-          <GridItem xs={12}>
-            <FastField
-              name='address'
-              render={args => {
-                return (
-                  <TextField label='Address' multiline rowsMax={3} {...args} />
-                )
-              }}
-            />
-          </GridItem>
-          <GridItem xs={12}>
+        </GridContainer>
+        <GridContainer>
+          <GridItem md={6}>
             <FastField
               name='subject'
               render={args => {
@@ -127,25 +54,200 @@ class ReferralLetter extends PureComponent {
               }}
             />
           </GridItem>
-
-          <GridItem xs={12} className={classes.editor}>
-            {templateLoader(
-              this.state.editorReferece,
-              setFieldValue,
-              currentType,
-            )}
-
+          <GridItem md={6}>
             <FastField
-              name='content'
+              name='generateDate'
               render={args => {
-                const cfg = {}
-                if (height && height > 495) {
-                  cfg.height = height - 545
-                }
+                return <DatePicker label='On' {...args} />
+              }}
+            />
+          </GridItem>
+        </GridContainer>
+        <div style={{ border: '0.5px solid #CCCCCC', margin: 8, padding: 8 }}>
+          <div style={{ fontWeight: 'bold' }}>Case History</div>
+          <FastField
+            name='caseHistory'
+            render={args => {
+              return (
+                <MultipleTextField
+                  label=''
+                  maxLength={2000}
+                  autoSize={{ minRows: 3 }}
+                  {...args}
+                />
+              )
+            }}
+          />
+        </div>
+
+        <div style={{ border: '0.5px solid #CCCCCC', margin: 8, padding: 8 }}>
+          <div style={{ fontWeight: 'bold' }}>Subjective Refraction</div>
+          <div style={{ lineHeight: '16px' }}>Left Eye (LE)</div>
+          <GridContainer>
+            <GridItem xs={2}>
+              <FastField
+                name='leftSPH'
+                render={args => {
+                  return <TextField maxLength={500} label='SPH' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='leftCYL'
+                render={args => {
+                  return <TextField maxLength={500} label='CYL' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='leftAXIS'
+                render={args => {
+                  return <TextField maxLength={500} label='AXIS' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='leftVA'
+                render={args => {
+                  return <TextField maxLength={500} label='VA' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='leftADD'
+                render={args => {
+                  return <TextField maxLength={500} label='ADD' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='leftPH'
+                render={args => {
+                  return <TextField maxLength={500} label='PH' {...args} />
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+          <div style={{ marginTop: 8, lineHeight: '16px' }}>Right Eye (RE)</div>
+          <GridContainer>
+            <GridItem xs={2}>
+              <FastField
+                name='rightSPH'
+                render={args => {
+                  return <TextField maxLength={500} label='SPH' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='rightCYL'
+                render={args => {
+                  return <TextField maxLength={500} label='CYL' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='rightAXIS'
+                render={args => {
+                  return <TextField maxLength={500} label='AXIS' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='rightVA'
+                render={args => {
+                  return <TextField maxLength={500} label='VA' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='rightADD'
+                render={args => {
+                  return <TextField maxLength={500} label='ADD' {...args} />
+                }}
+              />
+            </GridItem>
+            <GridItem xs={2}>
+              <FastField
+                name='rightPH'
+                render={args => {
+                  return <TextField maxLength={500} label='PH' {...args} />
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+        </div>
+
+        <div style={{ border: '0.5px solid #CCCCCC', margin: 8, padding: 8 }}>
+          <div style={{ fontWeight: 'bold' }}>Clinical Finding(s)</div>
+          <FastField
+            name='clinicalFindings'
+            render={args => {
+              return (
+                <MultipleTextField
+                  label=''
+                  maxLength={2000}
+                  autoSize={{ minRows: 3 }}
+                  {...args}
+                />
+              )
+            }}
+          />
+        </div>
+
+        <div style={{ border: '0.5px solid #CCCCCC', margin: 8, padding: 8 }}>
+          <div style={{ fontWeight: 'bold' }}>Reason(s) for referral</div>
+          <FastField
+            name='reasonsForReferral'
+            render={args => {
+              return (
+                <MultipleTextField
+                  label=''
+                  maxLength={2000}
+                  autoSize={{ minRows: 3 }}
+                  {...args}
+                />
+              )
+            }}
+          />
+        </div>
+
+        <GridContainer>
+          <GridItem xs={12}>
+            <FastField
+              name='degreeOfDemand'
+              render={args => {
                 return (
-                  <RichEditor
-                    editorRef={this.setEditorReference}
-                    {...cfg}
+                  <RadioGroup
+                    simple
+                    label=''
+                    defaultValue=''
+                    options={[
+                      {
+                        value: 'NonUrgent',
+                        label: 'Non-urgent (within 2-4 weeks)',
+                      },
+                      {
+                        value: 'Early',
+                        label: 'Early (within 2 weeks)',
+                      },
+                      {
+                        value: 'Urgent',
+                        label: 'Urgent (Same day)',
+                      },
+                      {
+                        value: 'Immediate',
+                        label: 'Immediate (attend A+E immediately)',
+                      },
+                    ]}
                     {...args}
                   />
                 )
@@ -153,13 +255,71 @@ class ReferralLetter extends PureComponent {
             />
           </GridItem>
         </GridContainer>
-        {footer &&
-          footer({
-            onConfirm: handleSubmit,
-            confirmBtnText: 'Save',
-          })}
       </div>
-    )
-  }
+      {footer &&
+        footer({
+          onConfirm: handleSubmit,
+          confirmBtnText: 'Save',
+        })}
+    </div>
+  )
 }
-export default ReferralLetter
+export default compose(
+  withFormik({
+    mapPropsToValues: ({
+      consultationDocument,
+      patient,
+      user,
+      visitRegistration,
+      corVisionRefraction = {},
+    }) => {
+      if (consultationDocument.entity) return consultationDocument.entity
+      const {
+        entity: { name = '' },
+      } = patient
+      const {
+        entity: { visit: { visitDoctor = [] } = {} } = {},
+      } = visitRegistration
+      const primaryDoctor = visitDoctor.find(d => d.isPrimaryDoctor)?.name
+      return {
+        type: consultationDocument.type,
+        patientName: name,
+        from: primaryDoctor,
+        to: '',
+        subject:
+          'Referral on Eye Examination at Singapore Polytechnic Optometry Centre',
+        generateDate: moment(),
+        issuedByUserFK: user.data.clinicianProfile.userProfileFK,
+        issuedByUser: user.data.clinicianProfile.name,
+        issuedByUserTitle: user.data.clinicianProfile.title,
+        leftSPH: corVisionRefraction.subjectiveRefraction_LE_SPH,
+        leftCYL: corVisionRefraction.subjectiveRefraction_LE_CYL,
+        leftAXIS: corVisionRefraction.subjectiveRefraction_LE_AXIS,
+        leftVA: corVisionRefraction.subjectiveRefraction_LE_VA,
+        leftADD: corVisionRefraction.subjectiveRefraction_NearAddition_LE_Value, //ADD
+        leftNVA: corVisionRefraction.subjectiveRefraction_NearAddition_LE_NVA, //NVA
+        rightSPH: corVisionRefraction.subjectiveRefraction_RE_SPH,
+        rightCYL: corVisionRefraction.subjectiveRefraction_RE_CYL,
+        rightAXIS: corVisionRefraction.subjectiveRefraction_RE_AXIS,
+        rightVA: corVisionRefraction.subjectiveRefraction_RE_VA,
+        rightADD:
+          corVisionRefraction.subjectiveRefraction_NearAddition_RE_Value, //ADD
+        rightNVA: corVisionRefraction.subjectiveRefraction_NearAddition_RE_NVA, //NVA
+      }
+    },
+    validationSchema: Yup.object().shape({}),
+    handleSubmit: (values, { props }) => {
+      const { dispatch, onConfirm, getNextSequence } = props
+      const nextSequence = getNextSequence()
+      dispatch({
+        type: 'consultationDocument/upsertRow',
+        payload: {
+          sequence: nextSequence,
+          ...values,
+        },
+      })
+      if (onConfirm) onConfirm()
+    },
+    displayName: 'ReferralLetter',
+  }),
+)(ReferralLetter)
